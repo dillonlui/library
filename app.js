4dbc88f8b304e4e915d79826eca7c8ce91144bf4
Dillon <76139920+dillonlui@users.noreply.github.com>
2021-03-16 15:42:10 -0500

    Added button to remove books from library

--- app.js
+++ app.js
@@ -37,6 +37,11 @@ function addBookToLibrary(newBook) {
     return true;
 }
 
+function removeBook(bookTitle) {
+    myLibrary = myLibrary.filter((book) => book.title !== bookTitle)
+    saveLocal();
+}
+
 // Book Grid
 
 function clearGrid() {
@@ -45,33 +50,58 @@ function clearGrid() {
     }
 }
 
+function finishedReading(value) {
+    if (value == true) {
+        return 'Book completed.'
+    } else {
+        return 'Book incomplete.'
+    }
+}
+
+function createCard(libraryBook) {
+    let book = libraryBook
+    let card = document.createElement('div');
+    card.className = 'card m-2';
+    card.style = 'width: 18rem';
+    let cardBody = document.createElement('div');
+    cardBody.className = 'card-body';
+    let cardTitle = document.createElement('div');
+    cardTitle.className = 'd-flex justify-content-between';
+    cardBody.appendChild(cardTitle);
+    let bookTitle = document.createElement('h5');
+    bookTitle.className = 'card-title';
+    bookTitle.textContent = `${book.title}`
+    cardTitle.appendChild(bookTitle);
+    let bookClose = document.createElement('button');
+    bookClose.type = 'button'
+    bookClose.className = 'btn-close closeBtn';
+    cardTitle.appendChild(bookClose);
+    let cardSubtitle = document.createElement('h6');
+    cardSubtitle.className = 'card-subtitle mb-2 text-muted';
+    cardSubtitle.textContent = `${book.author}`;
+    cardBody.appendChild(cardSubtitle);
+    let cardText = document.createElement('p');
+    cardText.className = 'card-text';
+    cardText.textContent = `${book.pages} pages. ${finishedReading(book.isRead)}`;
+    cardBody.appendChild(cardText);
+    card.appendChild(cardBody);
+    library.appendChild(card);
+}
+
 
 function showLibrary() {
     clearGrid(library);
     for (let i = 0; i < myLibrary.length; i++) {
-        let book = myLibrary[i]
-        let card = document.createElement('div');
-        card.className = 'card m-2';
-        card.style = 'width: 18rem';
-        let cardBody = document.createElement('div');
-        cardBody.className = 'card-body';
-        let cardTitle = document.createElement('h5');
-        cardTitle.className = 'card-title';
-        cardTitle.textContent = `${book.title}`
-        cardBody.appendChild(cardTitle);
-        let cardSubtitle = document.createElement('h6');
-        cardSubtitle.className = 'card-subtitle mb-2 text-muted';
-        cardSubtitle.textContent = `${book.author}`;
-        cardBody.appendChild(cardSubtitle);
-        let cardText = document.createElement('p');
-        cardText.className = 'card-text';
-        cardText.textContent = `${book.pages} pages. Finished reading? ${book.isRead}`;
-        cardBody.appendChild(cardText);
-        card.appendChild(cardBody);
-        library.appendChild(card);
+        createCard(myLibrary[i])
     }
 }
 
+library.addEventListener('click', function (e) {
+    if (e.target.classList.contains('closeBtn')) {
+        removeBook(e.target.previousElementSibling.innerText);
+        e.target.parentNode.parentNode.parentNode.parentNode.removeChild(e.target.parentNode.parentNode.parentNode);
+    }
+})
 // Modal Form
 
 let myModal = new bootstrap.Modal(document.getElementById('addBook'), {
